44cc3dde5e9cd9c6e749c07ddfed24c4c933f208
Rahil71 <115204249+Rahil71@users.noreply.github.com>
2024-04-06 06:20:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+// to show it is a newer version of javascript
+
+let name="Rahil"
+// string
+let age = 18
+// int
+let isAvailable = false
+// boolean
+let num=1221324521144243
+console.log(num)
+let nums=BigInt(12213245211442432242)
+console.log(nums)
+// bigint
+let a=null
+// null => standalone value
+let x
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
