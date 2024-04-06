02fcff70635b8fea93c0865269c0b73ecc8e53ec
Rahil71 <115204249+Rahil71@users.noreply.github.com>
2024-04-06 14:18:44 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// here the equality operator and comparison operator works differently
+// comparison operator converts null to 0 while the equality checks not that's why 3rd is true
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+// Also checks the datatype
+console.log("2" === 2);
