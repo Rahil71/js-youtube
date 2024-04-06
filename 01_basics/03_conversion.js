528660c77773da7e18ed69fb96e34ba2dacc0658
Rahil71 <115204249+Rahil71@users.noreply.github.com>
2024-04-06 11:23:10 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,48 @@
+let val=23
+console.log(val)
+console.log(typeof val)
+let toString1=String(val)
+console.log(toString1)
+console.log(typeof toString1)
+
+let val1="hitesh"
+console.log(val1)
+console.log(typeof val1)
+let toNum1=Number(val1)
+console.log(toNum1)
+console.log(typeof toNum1)
+
+let val2="23"
+console.log(val2)
+console.log(typeof val2)
+let toNum2=Number(val2)
+console.log(toNum2)
+console.log(typeof toNum2)
+
+let val3=null
+console.log(val3)
+console.log(typeof val3)
+let toNum3=Number(val3)
+console.log(toNum3)
+console.log(typeof toNum3)
+
+let val4
+console.log(val4)
+console.log(typeof val4)
+let toNum4=Number(val4)
+console.log(toNum4)
+console.log(typeof toNum4)
+
+let val5=true
+console.log(val5)
+console.log(typeof val5)
+let toNum5=Number(val5)
+console.log(toNum5)
+console.log(typeof toNum5)
+
+let val6="12ab"
+console.log(val6)
+console.log(typeof val6)
+let toNum6=Number(val6)
+console.log(toNum6)
+console.log(typeof toNum6)
